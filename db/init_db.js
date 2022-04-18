8ccd48829e5e78c7092b33cb836a099084332d44
Joel <joel.t.block@gmail.com>
2022-04-18 14:23:18 -0500

    created a couple tables, more to come

--- db/init_db.js
+++ db/init_db.js
@@ -8,6 +8,26 @@ async function buildTables() {
   try {
     client.connect();
 
+    await client.query(`
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+    
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE products(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      description VARCHAR(255),
+      price MONEY,
+      image VARCHAR(255)
+    );
+
+    `)
     // drop tables in correct order
 
     // build tables in correct order
@@ -16,8 +36,25 @@ async function buildTables() {
   }
 }
 
+// fetch product information from product api
+async function productFetch(){
+  try {
+    const response = await fetch("some api.com")
+    const data = response.json()
+    // optimize objects with correct key/value information
+    return data //array of objects
+  } catch (error) {
+    throw error
+  }
+}
+
 async function populateInitialData() {
   try {
+    const products = await productFetch()
+    products.map(async (product)=>{
+      return Products.createProductEntry(product)
+    })
+    
     // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
@@ -27,6 +64,6 @@ async function populateInitialData() {
 }
 
 buildTables()
-  .then(populateInitialData)
+  // .then(populateInitialData)
   .catch(console.error)
   .finally(() => client.end());
