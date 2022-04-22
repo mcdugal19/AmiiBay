303eaaba6a1ae6cb9c4413d7c276c340f6142d24
Joel <joel.t.block@gmail.com>
2022-04-22 13:15:19 -0500

    mostly finished NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,8 @@ import { AllProducts } from "./products";
 import { AddAdmin } from "./admin";
 
 import SingleProductCard from "./products/SingleProductCard";
-import { Login, Register, Logout } from "./";
-
+import { Login, Register } from "./";
+import { NavBar } from "./navbar";
 import { Checkout } from "./checkout";
 
 
@@ -21,6 +21,7 @@ const App = () => {
   return (
     <div className="app-container">
       <h1>Hello, World!</h1>
+      <NavBar />
       <p>API Status: {APIHealth}</p>
       <Routes>
         <Route path="/" element={<AllProducts />} />
@@ -28,7 +29,6 @@ const App = () => {
         {/* <Route path="/checkout" element={<Checkout />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/logout" element={<Logout />} />
         <Route path="/admin" element={<AddAdmin />} />
       </Routes>
     </div>
