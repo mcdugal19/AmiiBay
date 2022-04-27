3636bdf6f6b70ab6b3ee1369cd932e48f18bed5b
Derrick Hinh <derhin1@gmail.com>
2022-04-27 09:48:55 -0500

    Update Product in admin product fixed

--- src/components/admin/ProductTable.jsx
+++ src/components/admin/ProductTable.jsx
@@ -25,88 +25,97 @@ const ProductTable = () => {
   return (
     <div className="product-table">
       <table>
-        <tr className="table-headers">
-          <th>Product Name</th>
-          <th>Variation</th>
-          <th>Game</th>
-          <th>Image</th>
-          <th>Description</th>
-          <th>Price</th>
-          <th>Update Product</th>
-          <th>Delete Product</th>
-        </tr>
-        {products.map((product, idx) => {
-          return (
-            <tr className="product-table-content" key={idx}>
-              <td>{product.name}</td>
-              <td>{product.variation}</td>
-              <td>{product.game}</td>
-              <td>
-                <img
-                  src={product.image}
-                  width={"130px"}
-                  height={"130px"}
-                  alt={`${product.name} amiibo image`}
-                />
-              </td>
-              <td>{product.description}</td>
-              <td>{product.price}</td>
-              <td>
-                <form
-                  onSubmit={(event) => {
-                    event.preventDefault();
-                    // handleUpdateProduct(product.id);
-                  }}
-                >
-                  <button
-                    onClick={() => {
-                      handleShow();
-                      setProductId(product.id);
-                      setUpdateProductName(product.name);
-                      setUpdateProductDescription(product.description);
-                      setUpdateProductGame(product.game);
-                      setUpdateProductImage(product.image);
-                      setUpdateProductPrice(+product.price.slice(1));
-                      setUpdateProductVariation(product.variation);
+        <thead>
+          <tr className="table-headers">
+            <th>Product Name</th>
+            <th>Variation</th>
+            <th>Game</th>
+            <th>Image</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Update Product</th>
+            <th>Delete Product</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, idx) => {
+            return (
+              <tr className="product-table-content" key={idx}>
+                <td>{product.name}</td>
+                <td>{product.variation}</td>
+                <td>{product.game}</td>
+                <td>
+                  <img
+                    src={product.image}
+                    width={"130px"}
+                    height={"130px"}
+                    alt={`${product.name} amiibo image`}
+                  />
+                </td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>
+                  <form
+                    onSubmit={(event) => {
+                      event.preventDefault();
+                      // handleUpdateProduct(product.id);
                     }}
                   >
-                    Update Product
-                  </button>
-                </form>
-              </td>
-              <td>
-                <DeleteProduct productId={product.id} />
-              </td>
-            </tr>
-          );
-        })}
-        <Modal show={showModal} onHide={handleClose} className="modal">
-          <Modal.Header closeButton>
-            <Modal.Title>Update Product</Modal.Title>
-          </Modal.Header>
+                    <button
+                      onClick={() => {
+                        handleShow();
+                        setProductId(product.id);
+                        setUpdateProductName(product.name);
+                        setUpdateProductDescription(product.description);
+                        setUpdateProductGame(product.game);
+                        setUpdateProductImage(product.image);
+                        setUpdateProductPrice(+product.price.slice(1));
+                        setUpdateProductVariation(product.variation);
+                      }}
+                    >
+                      Update Product
+                    </button>
+                  </form>
+                </td>
+                <td>
+                  <DeleteProduct productId={product.id} />
+                </td>
+              </tr>
+            );
+          })}
+          <Modal show={showModal} onHide={handleClose} className="modal">
+            <Modal.Header>
+              <Modal.Title className="modal-title">Update Product</Modal.Title>
+            </Modal.Header>
 
-          <UpdateProduct
-            productId={productId}
-            setProductId={setProductId}
-            updateProductName={updateProductName}
-            setUpdateProductName={setUpdateProductName}
-            updateProductDescription={updateProductDescription}
-            setUpdateProductDescription={setUpdateProductDescription}
-            updateProductGame={updateProductGame}
-            setUpdateProductGame={setUpdateProductGame}
-            updateProductImage={updateProductImage}
-            setUpdateProductImage={setUpdateProductImage}
-            updateProductPrice={updateProductPrice}
-            setUpdateProductPrice={setUpdateProductPrice}
-            updateProductVariation={updateProductVariation}
-            setUpdateProductVariation={setUpdateProductVariation}
-          />
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+            <UpdateProduct
+              productId={productId}
+              setProductId={setProductId}
+              updateProductName={updateProductName}
+              setUpdateProductName={setUpdateProductName}
+              updateProductDescription={updateProductDescription}
+              setUpdateProductDescription={setUpdateProductDescription}
+              updateProductGame={updateProductGame}
+              setUpdateProductGame={setUpdateProductGame}
+              updateProductImage={updateProductImage}
+              setUpdateProductImage={setUpdateProductImage}
+              updateProductPrice={updateProductPrice}
+              setUpdateProductPrice={setUpdateProductPrice}
+              updateProductVariation={updateProductVariation}
+              setUpdateProductVariation={setUpdateProductVariation}
+              setShowModal={setShowModal}
+            />
+            <Modal.Footer>
+              <Button
+                className="modal-close-button"
+                variant="secondary"
+                onClick={handleClose}
+              >
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </tbody>
       </table>
     </div>
   );
